564b194cf0f77dbc2c77ba0d24eaee9ce3b228ad
esethugagasi <esethugagasi14@gmail.com>
2024-11-20 16:36:33 +0200

    installed fontawesome icons,  Display todo's added

--- my-todo-list/src/components/TodoWrapper.js
+++ my-todo-list/src/components/TodoWrapper.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
+import Todo from "./Todo";
 import { v4 as uuidv4 } from "uuid";
 uuidv4();
 
@@ -15,7 +16,11 @@ export default function TodoWrapper() {
   };
   return (
     <div className="TodoWrapper">
+      <h1>You can do this!</h1>
       <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} />
+      ))}
     </div>
   );
 }
