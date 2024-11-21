ed469de0dd35e89b139e1499615323879ab3dabb
esethugagasi <esethugagasi14@gmail.com>
2024-11-21 13:51:34 +0200

    Add background color, and text color

--- my-todo-list/src/components/TodoWrapper.js
+++ my-todo-list/src/components/TodoWrapper.js
@@ -46,7 +46,7 @@ export default function TodoWrapper() {
 
   return (
     <div className="TodoWrapper">
-      <h1>Get Things Done !</h1>
+      <h1>You can do this !</h1>
       <TodoForm addTodo={addTodo} />
       {/* display todos */}
       {todos.map((todo) =>
